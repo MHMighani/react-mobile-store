9b62ddbb931c127f593747f784d4893c079b5f98
MHMighani <mh.mgh2012@gmail.com>
2022-06-04 16:51:46 +0430

    Updates

---
+++ precache-manifest.0bb3b1ed1fdbe172fdf50d913155f36c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9e5e516598474582a429d64762f5de",
+    "url": "/react-mobile-store/index.html"
+  },
+  {
+    "revision": "001a31b821313cf40989",
+    "url": "/react-mobile-store/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "072cad3075bc390061b2",
+    "url": "/react-mobile-store/static/css/main.57656b2e.chunk.css"
+  },
+  {
+    "revision": "001a31b821313cf40989",
+    "url": "/react-mobile-store/static/js/2.1a23ba2b.chunk.js"
+  },
+  {
+    "revision": "072cad3075bc390061b2",
+    "url": "/react-mobile-store/static/js/main.ef0fb278.chunk.js"
+  },
+  {
+    "revision": "166d209d4f1261106ffb",
+    "url": "/react-mobile-store/static/js/runtime-main.6628221e.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/react-mobile-store/static/media/logo.9f7e506d.svg"
+  }
+]);
